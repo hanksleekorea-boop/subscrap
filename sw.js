db22d48544b8e59a2b3d7f23d0940ee3b2a906d6
hanksleekorea-boop <304308135+hanksleekorea-boop@users.noreply.github.com>
2026-08-14 12:35:33 +0700

    Publish Subscrap v0.17.0

---
+++ sw.js
@@ -0,0 +1,7 @@
+const C="subscrap-0.17.0";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html"]).catch(()=>{})))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+  const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl).catch(()=>{}));return res;
+ }).catch(()=>caches.match("./index.html"))));});
