776a2b065639b6e2f3f5e4534920f0b36e376dd4
hanksleekorea-boop <hanksleekorea@gmail.com>
2026-08-14 23:53:03 +0700

    Publish Subscrap v0.19.1 cache update fix

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const C="subscrap-0.19.0";
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html"]).catch(()=>{})))});
+const C="subscrap-0.19.1";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./index.html"]).catch(()=>{})))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-  const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl).catch(()=>{}));return res;
- }).catch(()=>caches.match("./index.html"))));});
+ const u=new URL(e.request.url);if(u.origin!==self.location.origin)return;
+ e.respondWith(fetch(e.request,{cache:"no-store"}).then(res=>{
+  if(res&&res.ok){const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl).catch(()=>{}))}return res;
+ }).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));});
