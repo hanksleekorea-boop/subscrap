0a6ba2ae55e563966f836a2b0815d1f6f9575213
hanksleekorea-boop <hanksleekorea@gmail.com>
2026-08-19 19:46:16 +0700

    Expand-bundle-recommendations-to-20 (#2)
    Co-authored-by: hanksleekorea-boop <304308135+hanksleekorea-boop@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="subscrap-0.20.1";
+const C="subscrap-0.20.2";
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./index.html"]).catch(()=>{})))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;
